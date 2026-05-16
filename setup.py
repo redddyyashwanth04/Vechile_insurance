3c4fb803c3a24ce1cf0bec6f671c85930372c964
redddyyashwanth04 <yashwanthreddy0415@gmail.com>
2026-05-16 18:41:26 +0530

    project structure implemented

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    author="yashwanth",
+    author_email="yashwanthreddy0415@gmail.com",
+    packages=find_packages()
+)
